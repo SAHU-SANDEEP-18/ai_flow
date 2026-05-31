6399df7ce387481b65490ffa52817db0172ad820
Sandeepsahu18 <editingking998@gmail.com>
2026-05-31 19:51:14 +0530

    remember the last mesaage

--- index.js
+++ index.js
@@ -1,47 +1,52 @@
-import "dotenv/config"
+import "dotenv/config";
 import readline from "readline";
 import { ChatMistralAI } from "@langchain/mistralai";
+import { HumanMessage } from "langchain";
 
+// Create readline interface
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
+// Wrap rl.question in a Promise
+function askQuestion(query) {
+  return new Promise((resolve) => {
+    rl.question(query, (answer) => resolve(answer));
+  });
+}
+
+// Initialize Mistral model
 const model = new ChatMistralAI({
-    model: "mistral-small-latest"
-})
+  model: "mistral-small-latest",
+});
 
-const response = await model.invoke("what is the capital of India")
+// Store conversation messages
+const messages = [];
 
-console.log(response.content)
+async function main() {
+  while (true) {
+    const userInput = await askQuestion("\x1b[32mYou:\x1b[0m ");
+    if (userInput.toLowerCase() === "exit") break; // graceful exit
 
+    messages.push(new HumanMessage(userInput));
+    const response = await model.invoke(messages);
+    messages.push(response);
 
-rl.close()
+    console.log(`\x1b[34m[AI]:\x1b[0m ${response.content}`);
+  }
 
+  rl.close();
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+main();
 
 
+// const response = await model.invoke("what is the capital of India")
 
+// console.log(response.content)
 
-
-
-
-
-
-
+// rl.close()
 
 // function askQuestion(question) {
 //   return new Promise((resolve) => {
@@ -63,4 +68,3 @@ rl.close()
 //   console.log(`Hello ${name}!`);
 //   rl.close();
 // });
-
